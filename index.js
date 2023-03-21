ca9f1f65fcb573aade4c1b7b86f3b8de0809431b
mannybennett <mannybennett99@gmail.com>
2023-03-20 21:51:29 -0400

    finished

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/users', (req, res) => {
   res.json(users)
 })
 
+// app.get('/users/1', (req, res) => {
+//   res.json(users[0])
+// })
+
 app.post('/users', (req, res) => {
   users.push({
     "_id": 6,
@@ -28,6 +32,16 @@ app.post('/users', (req, res) => {
   users.push(req.body)
   res.json(users.at(-1))
 })
+
+// app.put('/users/1', (req, res) => {
+//     users[0].name = "Jimmy John"
+//     res.json(users[0])
+//   })
+  
+// app.delete('/users/1', (req, res) => {
+//   users.splice(0, 1)
+//   res.send("Deleted")
+// })
     
 /* END - create routes here */
     

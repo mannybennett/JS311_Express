2dac760dfe175fc6462d69ecccc8d43bb6a0b7ec
mannybennett <mannybennett99@gmail.com>
2023-03-20 21:46:52 -0400

    finished

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
-
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
+  
+app.use(express.json())
 
 const { users } = require('./state')
+const counter = users.length + 1
 
 /* BEGIN - create routes here */
 
+app.get('/users', (req, res) => {
+  res.json(users)
+})
 
+app.post('/users', (req, res) => {
+  users.push({
+    "_id": 6,
+    "name": "Yee Haw",
+    "occupation": "Cowboy",
+    "avatar": "https://upload.wikimedia.org/wikipedia/en/5/50/Agentdalecooper.jpg"
+  })
+  res.json(users.at(-1))
+})
+
+app.post('/users', (req, res) => {
+  req.body = { "_id": counter }
+  users.push(req.body)
+  res.json(users.at(-1))
+})
+    
 /* END - create routes here */
+    
+app.get('/users/:userId', (req, res) => {
+  let user = req.params.userId
+  res.json(users.find((obj) => obj._id === parseInt(user)))
+})
+  
+app.put('/users/:userId', (req, res) => {
+  let user = req.params.userId
+  users[parseInt(user) - 1].name = "Jimmy John"
+  res.json(users[parseInt(user) - 1])
+})
 
-app.listen(port, () => 
-  console.log(`Example app listening on port ${port}!`))
+app.delete('/users/:userId', (req, res) => {
+  let user = req.params.userId
+  let removedUser = users.slice(user - 1, user)
+  removedUser.isActive = false
+  users.splice(user - 1, 1)
+  console.log(removedUser)
+  res.send("Deleted")
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
